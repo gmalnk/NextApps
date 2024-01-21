1a57e0e23ab5de0cab92ee7c2fd702beab612ba9
ANIL NAYAK <goramnayak5@gmail.com>
2024-01-21 17:24:36 +0530

    updating the projects to direct the deployments on click

--- apps/portfolio/app/layout.tsx
+++ apps/portfolio/app/layout.tsx
@@ -25,13 +25,11 @@ export default function RootLayout({
         lang="en"
         className="!scroll-smooth bg-gray-50 text-gray-950 m-0 p-0 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90"
       >
-        <body className="bg-gray-50 text-gray-950 m-0 p-0 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90">
+        <body className="bg-gray-50  h-screen w-screen text-gray-950 m-0 p-0 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90">
           <div className=" bg-gray-50 text-gray-950  dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 relative pt-28 sm:pt-44">
             <Header />
             {children}
             <Footer />
-
-            {/* <Toaster position="top-right" /> */}
             <ThemeSwitchButton />
           </div>
         </body>
