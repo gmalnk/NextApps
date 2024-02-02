1efd05b79f1faca9b2510082777eea88d019f162
ANIL NAYAK <goramnayak5@gmail.com>
2024-02-02 23:45:34 +0530

    getting free fron autoprefixer paradox

--- apps/series-clone-apps-netflix/src/app/layout.tsx
+++ apps/series-clone-apps-netflix/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "../../components/NextAuthProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Netflix-clone",
@@ -18,7 +18,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}> */}
+      <body>
         <NextAuthProvider>{children}</NextAuthProvider>
       </body>
     </html>
