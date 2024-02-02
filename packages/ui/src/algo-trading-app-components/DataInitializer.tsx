a95dc2aea897fd34bddad942ad768468b90e1301
ANIL NAYAK <goramnayak5@gmail.com>
2024-02-02 18:51:25 +0530

    type errors in datainitializer resolved

--- packages/ui/src/algo-trading-app-components/DataInitializer.tsx
+++ packages/ui/src/algo-trading-app-components/DataInitializer.tsx
@@ -6,15 +6,28 @@ import { useRecoilState, useSetRecoilState } from "recoil";
 import { userWishListState } from "store/atoms/trend.io";
 import { getWishListData } from "utils/trend.io";
 
+type User = {
+  id?: string;
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+};
+
+type Session = {
+  user?: User | null | undefined;
+};
+
 export default function DataInitializer() {
   const setUserId = useSetRecoilState(userIdState);
   const setWishlist = useSetRecoilState(userWishListState);
 
   // Assuming useSession provides the user object with an 'id' property
-  const { data: session } = useSession();
+  const { data: session } = useSession() as {
+    data?: Session | null | undefined;
+  };
 
   useEffect(() => {
-    const userId = session?.user?.id || null;
+    const userId = session?.user?.id as string;
 
     const setWishListForUser = async () => {
       const wishList = await getWishListData(userId);
