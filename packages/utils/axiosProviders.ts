37a5e770cb1c24173890a984c22d026772f9e309
ANIL NAYAK <goramnayak5@gmail.com>
2024-02-04 19:05:11 +0530

    certbot ssl certificate integrated with a free sub domain

--- packages/utils/axiosProviders.ts
+++ packages/utils/axiosProviders.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const trendIOAPI = axios.create({
-  baseURL: "http://13.126.174.34/api/v1/trend.io",
+  baseURL: "https://trend.io.kstar.us/api/v1/trend.io",
 });
